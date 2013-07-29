714ab540f3f734a63b0c8c1ec7c451e5db331570
Mads Ohm Larsen <mads.ohm@gmail.com>
2013-07-29 18:04:18 +0200

    Cleared level 4

--- ohm-beginner/player.rb
+++ ohm-beginner/player.rb
@@ -1,13 +1,19 @@
 class Player
+  def initialize
+    @health = 20
+  end
+
   def play_turn(warrior)
     space = warrior.feel
 
     if space.enemy?
       warrior.attack!
-    elsif warrior.health < 7
+    elsif warrior.health < 20 and @health <= warrior.health
       warrior.rest!
     else
       warrior.walk!
     end
+
+    @health = warrior.health
   end
 end
