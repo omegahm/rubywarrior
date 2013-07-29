bda4b124b4529c3d0c466750f03fcf3848790801
Mads Ohm Larsen <mads.ohm@gmail.com>
2013-07-29 18:13:04 +0200

    Cleared level 6

--- ohm-beginner/player.rb
+++ ohm-beginner/player.rb
@@ -1,21 +1,43 @@
 class Player
   def initialize
     @health = 20
+    @direction = :backward
+    @attacking = false
+    @resting = false
   end
 
   def play_turn(warrior)
-    space = warrior.feel
+    space = warrior.feel @direction
+
+    if space.wall?
+      @direction = :forward
+    end
 
     if space.captive?
-      warrior.rescue!
+      warrior.rescue! @direction
+      @resting = false
     elsif space.enemy?
-      warrior.attack!
+      warrior.attack! @direction
+      @resting = false
+      @attacking = true
     elsif space.stairs?
-      warrior.walk!
-    elsif warrior.health < 20 and @health <= warrior.health
-      warrior.rest!
+      warrior.walk! @direction
     else
-      warrior.walk!
+      if warrior.health < 20 and @health <= warrior.health
+        warrior.rest!
+
+        if @resting and @direction == :backward
+          @direction = :forward
+        end
+
+        @resting = true
+      else
+        warrior.walk! @direction
+        @resting = false
+      end
+
+      @direction = :backward if @attacking
+      @attacking = false
     end
 
     @health = warrior.health
