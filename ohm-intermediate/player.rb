36e343b11d469d83bc21c6aca7febeb31132439b
Mads Ohm Larsen <mads.ohm@gmail.com>
2013-07-29 18:58:04 +0200

    Intermediate - Cleared level 2

--- ohm-intermediate/player.rb
+++ ohm-intermediate/player.rb
@@ -1,6 +1,25 @@
 class Player
   def play_turn(warrior)
     stairs_direction = warrior.direction_of_stairs
-    warrior.walk! stairs_direction
+    
+    # feel all around
+    spaces = {
+      :forward  => warrior.feel(:forward),
+      :left     => warrior.feel(:left),
+      :right    => warrior.feel(:right),
+      :backward => warrior.feel(:backward)
+    } 
+
+    enemies = spaces.select {|k, v| v.enemy? }
+
+    if enemies.empty?
+      if warrior.health < 20
+        warrior.rest!
+      else
+        warrior.walk! stairs_direction
+      end
+    else
+      warrior.attack! enemies.first.first # first gets the first enemy, first get the direction
+    end
   end
 end
